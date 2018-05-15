64adc88475422a3dea1f70ecaea48cf3b273719c
EruditePig <EruditePig@users.noreply.github.com>
2018-05-15 23:51:51 +0800

    Create SimplifyWebsiteForSave.js

---
+++ SimplifyWebsiteForSave.js
@@ -0,0 +1,83 @@
+// ==UserScript==
+// @name            简化网站以存储
+// @namespace       http://tampermonkey.net/
+// @description     Test
+// @version         0.1
+// @author          author
+// @include         *
+// @exclude         file://*
+// @require         http://code.jquery.com/jquery-1.11.0.min.js
+// @grant           GM_registerMenuCommand
+// ==/UserScript==]
+
+
+(function() {
+    'use strict';
+
+function test() {
+	$("body > div.page-container").siblings().remove();
+	$("body").children().children(":not(.head-container,.article-holder)").remove();
+}
+
+function csdn_blog(){
+    $("body > div.container").siblings().remove();
+    $("main:first").siblings().remove();
+    $("article:first").siblings().remove();
+}
+
+function jianshu(){
+    $("body > div.note").siblings().remove();
+    $("div.post:first").siblings().remove();
+    $("div.article:first").siblings().remove();
+}
+function zhihu_daily(){
+    var del = document.getElementsByClassName("global-header");
+    del[0].parentElement.removeChild(del[0])
+    del = document.getElementsByClassName("header-for-mobile");
+    del[0].parentElement.removeChild(del[0])
+    del = document.getElementsByClassName("bottom-wrap");
+    del[0].parentElement.removeChild(del[0])
+    del = document.getElementsByClassName("footer");
+    del[0].parentElement.removeChild(del[0])
+    del = document.getElementsByClassName("qr");
+    del[0].parentElement.removeChild(del[0])
+    del = document.getElementsByClassName("question");
+    del[1].parentElement.removeChild(del[1])
+}
+
+function bilibili() {
+	$("body > div.page-container").siblings().remove();
+	$("body").children().children(":not(.head-container,.article-holder)").remove();
+}
+
+function codeproject() {
+	$("body > div.page-background").siblings().remove();
+	$("div#A").siblings().remove();
+	$("table.container-article-parts").siblings().remove();
+	$("td.article-wing-left").remove();
+	$("td.article-wing-right").remove();
+	$("div#AT").children(":not(.article)").remove();
+}
+
+
+function simplify(){
+    var dict = {};
+    dict["^(?:http(s)?:\/\/)?blog\.csdn\.net\/.+"] = "csdn_blog";      // csdn blog
+    dict["^(?:http(s)?:\/\/)?daily\.zhihu\.com\/.+"] = "zhihu_daily";      // zhihu_daily
+    dict["^(?:http(s)?:\/\/)?www\.jianshu\.com\/.+"] = "jianshu";      // jianshu
+    dict["^(?:http(s)?:\/\/)?www\.bilibili\.com\/.+"] = "bilibili";      // bilibili
+    dict["^(?:http(s)?:\/\/)?www\.codeproject\.com\/.+"] = "codeproject";      // codeproject
+    for(var key in dict) {
+        var reg = new RegExp(key);
+        if(reg.test(document.URL)){
+            var value = dict[key];
+            eval(value + '()');
+            return;
+        }
+    }
+    alert("没有匹配的简化代码");
+}
+
+GM_registerMenuCommand("simplify", simplify, "h");
+
+})();
