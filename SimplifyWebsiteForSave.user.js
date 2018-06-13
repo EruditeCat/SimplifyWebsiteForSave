3d8b63d0a5e4a878687468bcee7f6a2bd0b77d48
EruditePig <EruditePig@users.noreply.github.com>
2018-06-13 18:32:21 +0800

    Update SimplifyWebsiteForSave.user.js
    增加知乎专栏的支持

--- SimplifyWebsiteForSave.user.js
+++ SimplifyWebsiteForSave.user.js
@@ -2,7 +2,7 @@
 // @name            简化网站以存储
 // @namespace       http://tampermonkey.net/
 // @description     Test
-// @version         0.2.4
+// @version         0.2.5
 // @author          EruditePig
 // @include         *
 // @exclude         file://*
@@ -48,6 +48,14 @@ function zhihu_daily(){
     del[1].parentElement.removeChild(del[1])
 }
 
+function zhihu_zhuanlan(){
+    remove_nodes(siblings(document.getElementById("root")));
+    remove_nodes(siblings(document.getElementsByClassName("App-main")[0]));
+    remove_nodes(siblings(document.getElementsByClassName("Post-Main Post-NormalMain")[0]));
+    remove_nodes(document.getElementsByClassName("Recommendations-Main"));
+    remove_nodes(document.getElementsByClassName("Comments-container"));
+}
+	
 function bilibili() {
 	$("body > div.page-container").siblings().remove();
 	$("body").children().children(":not(.head-container,.article-holder)").remove();
@@ -71,14 +79,15 @@ function ruanyifeng() {
     $("div.entry-sponsor").remove();
 }
 
+	
 function siblings(node){
     var ss = [].slice.call(node.parentNode.children).filter(function(v){return v!== node});
     return ss;
 }
 	
 function remove_nodes(nodes){
-    for (var node in nodes) {
-        node.parentElement.removeChild(node);
+    for (var i=0; i<nodes.length; i++) {
+        nodes[i].parentElement.removeChild(nodes[i]);
     }
 }
 	
@@ -89,6 +98,7 @@ function simplify(){
     var dict = {};
     dict["^(?:http(s)?:\/\/)?blog\.csdn\.net\/.+"] = "csdn_blog";      // csdn blog
     dict["^(?:http(s)?:\/\/)?daily\.zhihu\.com\/.+"] = "zhihu_daily";      // zhihu_daily
+    dict["^(?:http(s)?:\/\/)?zhuanlan\.zhihu\.com\/.+"] = "zhihu_zhuanlan";      // zhihu_zhuanlan
     dict["^(?:http(s)?:\/\/)?www\.jianshu\.com\/.+"] = "jianshu";      // jianshu
     dict["^(?:http(s)?:\/\/)?www\.bilibili\.com\/.+"] = "bilibili";      // bilibili
     dict["^(?:http(s)?:\/\/)?www\.codeproject\.com\/.+"] = "codeproject";      // codeproject
