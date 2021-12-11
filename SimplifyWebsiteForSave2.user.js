f76ebb694f4f63410a338e779a3acfa8c284fb83
shijianxin <shijianxin@cabrtech.com>
2021-12-11 12:55:39 +0800

    增加版本号

--- SimplifyWebsiteForSave2.user.js
+++ SimplifyWebsiteForSave2.user.js
@@ -2,7 +2,7 @@
 // @name            简化网站以存储2
 // @namespace       http://tampermonkey.net/
 // @description     重写的简化网站以存储
-// @version         1.0.2
+// @version         1.1.0
 // @author          EruditePig
 // @include         *
 // @exclude         file://*
