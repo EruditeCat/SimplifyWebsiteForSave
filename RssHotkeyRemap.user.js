fcc68be799ce9f169b4eeb0307b8cfbc66322991
shijianxin <shijianxin@cabrtech.com>
2022-04-04 17:35:17 +0800

    【feature】把inoreader的css配置也放到代码里

--- RssHotkeyRemap.user.js
+++ RssHotkeyRemap.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Rss快捷键映射
 // @namespace    http://EruditePig.net/
-// @version      0.1
+// @version      0.2
 // @description  Inoreader和the old reader快捷键映射，利用小键盘区域，方便快速浏览文章
 // @author       EruditePig
 // @match        https://www.inoreader.com/*
@@ -55,6 +55,66 @@
         }
     };
 
-
+    // 加载css
+    function loadCss() {
+        let style = document.createElement('style');
+        style.innerHTML = `
+        /*让open之后预览下，旁边的黑色遮罩别那么黑*/
+        #article_dialog_modal_overlay{
+            opacity: 0.6 !important ; 
+        }
+        
+        /*不显示中国政府新闻的logo*/
+        div.article_content p img[alt="20190530videobg.jpg"],
+        /*不显示日经新闻的logo*/
+        div.article_content p img[alt="nikkei-chinese-logo-02.jpg"],
+        /*不显示小众软件论坛delogo*/
+        div.article_content p img[alt="cbd4ea4a240cf3eb8809fb647da09ba01691f6b4"]
+        {
+            display:none !important;
+        }
+        
+        /*右侧按钮调整到左侧*/
+        a[id^="aurl"],
+        div[class~="popularity_div"]{
+            display:none !important;
+        }
+        div[class~="article_unread_dot"],
+        div[class~="article_favicon"],
+        div[class~="arrow_div"]{
+            order : -1;
+        }
+        
+        div[class~="arrow_div"]{
+            position:relative !important;
+            float:left !important;
+            /*padding-left:0px !important;*/
+            padding-top: 4px !important;
+            /*left: -5px  !important;*/
+        }
+        
+        /*列表模式下，扩大阅读文章宽度到1000px*/
+        div[class~="article_expanded"] div[class="article_full_contents"]{
+            width:1000px !important;
+        }
+        div[class~="article_expanded"] div[class="article_full_contents"] div[class="article_title"]{
+            width:1000px !important;
+        }
+        div[class~="article_expanded"] div[class="article_full_contents"] div.article_content{
+            width:1000px !important;
+            max-width:1000px !important;
+        }
+        div.article_content p img ,
+        div.article_content img 
+        {
+            display: block !important;
+            margin-left: auto !important;
+            margin-right: auto !important;
+        }
+        
+        `;
+        document.body.appendChild(style);
+    };
+    loadCss();
 
 })();
